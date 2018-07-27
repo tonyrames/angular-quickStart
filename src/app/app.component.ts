fa2515b2387ac247b117ed533d6c9ca841a7eaf5
tonyrames <tonyramesh45@gmail.com>
2018-07-27 16:46:54 +0530

    Add: Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Hello World</h1>
+  <router-outlet></router-outlet>
   `
 })
 export class AppComponent {
